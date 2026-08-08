8d5f5a367cbf42a6acd8e433be6940d972a1f5f1
gus corion <guscorion@guss-Mac-mini.local>
2026-08-08 11:26:55 +0100

    Use enlarged heart icon branding

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='caring-hands-manager-hub-v7';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./caring-hands-logo.png','./icon-192.png','./icon-512.png'];
+const CACHE='caring-hands-manager-hub-v8';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./heart-home-icon.png','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{if(event.request.method==='GET')event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(c=>c.put(event.request,copy));return response})))})
