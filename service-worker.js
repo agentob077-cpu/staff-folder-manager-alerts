98987505f570cecbf6c7cce581060cc3628b698c
gus corion <guscorion@guss-Mac-mini.local>
2026-08-08 13:02:40 +0100

    Fill home screen icon with Caring Hands heart

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='caring-hands-manager-hub-v9';
+const CACHE='caring-hands-manager-hub-v10';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./heart-home-icon.png','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
